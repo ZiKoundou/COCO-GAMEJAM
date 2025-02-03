24b1ae3d7bd512a64bd7456994e562a7b198850a
Zion Koundou <114265782+ZiKoundou@users.noreply.github.com>
2025-02-03 14:43:36 -0800

    basic movement animations

--- Assets/Sciripts/playermovement.cs
+++ Assets/Sciripts/playermovement.cs
@@ -9,19 +9,34 @@ public class playermovement : MonoBehaviour
     private int speed = 4;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
+    public bool still;
     // Start is called before the first frame update
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnMovement(InputValue value){
         movement = value.Get<Vector2>();
+        animator.SetFloat("X", movement.x);
+        animator.SetFloat("Y", movement.y);
     }
     // Update is called once per frame
+
+
     void Update()
     {
+        Move();
+        
+
+        
+        
+    }
+
+    void Move(){
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
